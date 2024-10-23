779ff393983af31e5e7e434cefae2b8e800939dd
coded-with-claws <79459323+coded-with-claws@users.noreply.github.com>
2024-10-23 23:24:01 +0200

    x & y offset

--- MiSTer_SSD1322_ST7789_USB/MiSTer_SSD1322_ST7789_USB.ino
+++ MiSTer_SSD1322_ST7789_USB/MiSTer_SSD1322_ST7789_USB.ino
@@ -232,6 +232,8 @@
   U8G2_FOR_ADAFRUIT_GFX u8g2;
   #define OLED_WHITE SSD1322_WHITE
   #define OLED_BLACK SSD1322_BLACK
+  #define X_OFFSET 0
+  #define Y_OFFSET 0
 #endif
 
 
@@ -241,7 +243,8 @@
   //Adafruit_ST7789 oled = Adafruit_ST7789(OLED_CS, OLED_DC, OLED_MOSI, OLED_SCLK, OLED_RESET);
   #define OLED_WHITE ST77XX_WHITE
   #define OLED_BLACK ST77XX_BLACK
-  #define ST7789_Y_OFFSET 80
+  #define X_OFFSET 1
+  #define Y_OFFSET 80
 #endif
 
 // Tilt Sensor
@@ -431,7 +434,7 @@ void oled_drawgreyscale(const uint8_t);
 
 // Info about overloading found here
 // https://stackoverflow.com/questions/1880866/can-i-set-a-default-argument-from-a-previous-argument
-inline void oled_drawEightPixelXY(int x, int y) { oled_drawEightPixelXY(x,y,x,y); };
+inline void oled_drawEightPixelXY(int x, int y) { oled_drawEightPixelXY(x-X_OFFSET,y+Y_OFFSET,x,y); };
 
 // =============================================================================================================
 // ================================================ SETUP ======================================================
@@ -1010,15 +1013,7 @@ void oled_showStartScreen(void) {
   Serial.println(F("Show Startscreen"));
 #endif
   oled_cleardisplay();
-
-#ifdef XSSD1322
-  oled.drawXBitmap(82, 0, tty2oled_logo, tty2oled_logo_width, tty2oled_logo_height, OLED_WHITE);
-#endif
-
-#ifdef XST7789
-  oled.drawXBitmap(82, ST7789_Y_OFFSET, tty2oled_logo, tty2oled_logo_width, tty2oled_logo_height, OLED_WHITE);
-#endif
-
+  oled.drawXBitmap(82, Y_OFFSET, tty2oled_logo, tty2oled_logo_width, tty2oled_logo_height, OLED_WHITE);
   oled_display();
   delay(1000);
   for (int i=0; i<DispWidth; i+=16) {            // Some Animation
@@ -1055,13 +1050,7 @@ void oled_showStartScreen(void) {
   //u8g2.setFont(u8g2_font_5x7_mf);               // 6 Pixel Font
   oled_setfont(0);
 
-#ifdef XSSD1322
-  oled_setcursor(0,63);
-#endif
-
-#ifdef XST7789
-  oled_setcursor(0,ST7789_Y_OFFSET+60);
-#endif
+  oled_setcursor(0,Y_OFFSET+63);
 
   oled_printtext(BuildVersion);
   if (runsTesting) {
@@ -1621,15 +1610,7 @@ void oled_sendHardwareInfo(void) {
 // --------------------------------------------------------------
 void oled_drawlogo64h(uint16_t w, const uint8_t *bitmap) {
   oled_cleardisplay();
-
-#ifdef XSSD1322
-  oled.drawXBitmap(DispWidth/2-w/2, 0, bitmap, w, DispHeight, OLED_WHITE);
-#endif
-
-#ifdef XST7789
-  oled.drawXBitmap(DispWidth/2-w/2, ST7789_Y_OFFSET, bitmap, w, DispHeight, OLED_WHITE);
-#endif
-
+  oled.drawXBitmap(DispWidth/2-w/2, Y_OFFSET, bitmap, w, DispHeight, OLED_WHITE);
   oled_display();
 } // end oled_drawlogo64h
 
@@ -1926,14 +1907,7 @@ int8_t oled_getFontAscent(void) {
 // -------------------------- Draw a 1-Bit Monochrome Picture--------------
 // -----------------------------------------------------------------------
 void oled_drawmonochrome(const uint8_t bitmap[]) {
-
-#ifdef XSSD1322
-  oled.drawXBitmap(0, 0, bitmap, DispWidth, DispHeight, OLED_WHITE);
-#endif
-
-#ifdef XST7789
-  oled.drawXBitmap(0, ST7789_Y_OFFSET, bitmap, DispWidth, DispHeight, OLED_WHITE);
-#endif
+  oled.drawXBitmap(X_OFFSET, Y_OFFSET, bitmap, DispWidth, DispHeight, OLED_WHITE);
 }
 
 // -----------------------------------------------------------------------
@@ -1948,12 +1922,16 @@ int x, y;
 #endif
 
 #ifdef XST7789
-  for (x=0; x<DispLineBytes1bpp; x++) {
-        for (y=0; y<DispHeight; y++) {
+
+// Draw top to bottom
+
+for (y=0; y<DispHeight; y++) {
+        for (x=0; x<DispLineBytes1bpp; x++) {
           oled_drawEightPixelXY(x, y);
         }
-        oled_display();
+        if (y%2==1) oled_display();
       }
+
 #endif
 }
 
